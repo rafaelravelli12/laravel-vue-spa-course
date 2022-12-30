55762fb6e71e1478852a2e94719985ae17e4a16a
rafaelravelli12 <rafaelravelli1@gmail.com>
2022-12-30 12:24:21 -0300

    Class 175 working

---
+++ laravel/resources/js/app.js
@@ -0,0 +1,18 @@
+require('./bootstrap');
+
+import VueRouter from "vue-router";
+import router from "./routes";
+
+window.Vue = require('vue');
+
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
+
+Vue.use(VueRouter);
+
+const app = new Vue({
+    el: "#app",
+    router,
+});
