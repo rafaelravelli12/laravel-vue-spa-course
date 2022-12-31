1fde24e323bfcea523e49c864ca5fbd9a5c2c42d
rafaelravelli12 <rafaelravelli1@gmail.com>
2022-12-30 22:26:33 -0300

    Class 178 - Some errors in dev.chrome

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,20 +1,24 @@
 require('./bootstrap');
 
 import VueRouter from "vue-router";
+import Index from "./Index";
 import router from "./routes";
 
 window.Vue = require('vue');
 
-Vue.component(
-    "example-component",
-    require("./components/ExampleComponent.vue").default
-);
+// Vue.component(
+//     "example-component",
+//     require("./components/ExampleComponent.vue").default
+// );
 
-Vue.component("example-2", require("./components/Example2.vue").default);
+// Vue.component("example-2", require("./components/Example2.vue").default);
 
 Vue.use(VueRouter);
 
 const app = new Vue({
     el: "#app",
     router,
+    components: {
+        index: Index,
+    },
 });
